30e7ccb74712e9dfe61cb688c59a10ee99baf689
ryan goo <ryankgoo@gmail.com>
2016-09-28 13:33:47 -0700

    DB Config changes

--- config/database.php
+++ config/database.php
@@ -54,8 +54,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '33060'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
@@ -64,7 +64,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock'
+            
         ],
 
         'pgsql' => [
